b6405fe9f3f221306ac5d789dfba9f10b3fd5874
Raghav Mehrotra <raghavmehrotra@Raghavs-MacBook-Air.local>
2025-11-30 21:32:16 -0600

    Updated milestone, cleaned up code

--- milestones/interactive-draft.md
+++ milestones/interactive-draft.md
@@ -5,11 +5,13 @@ Raghav Mehrotra
 ## Goal
 
 My goal is to allow a user to explore the Indian Lok Sabha's Question Hour data for slices of the data that pertain to them.
-This personalization currently takes two main forms -- it allows the user to understand how MPs from a particular state have participated in the QH over time, as well as how the user's topic compares to certain existing topics/themes.
+This personalization currently takes two main forms -- it allows the user to understand how MPs from a particular state have participated in the QH over time, as well as how the user's topic of interest compares to certain existing topics/themes that were discussed from 2000-2018.
 
-I am hoping to extend this to include a geospatial visualization, where users can interact with a clickable map to see all the questions that MPs from a particular constituency have asked from 2000-2018. 
+I am hoping to extend this to include a geospatial visualization, where users can interact with a clickable map to see all the questions that MPs from a particular constituency have asked from 2000-2018. I have more details on this below.
 
-I had initally wanted to do a scrollytelling article, but I will focus more of my efforts on the map.
+I had initally wanted to do a scrollytelling article, but I will focus more of my efforts on the map. In its current form, the webpage corresponds to the Option B ensemble in the assignment. With the map, it will be Option B + C.
+
+The webpage currently has placeholder text/descriptions, which I will update after writing all the code.
 
 ## Data Challenges
 For the text search, I am currently using a mini dataset that only searches for the subject of a question, not the entire question or answer text. I did this for two reasons: (1) to get the prototype and D3 working (2) because the dataset itself is ~300k rows and wasn't sure if I would have rendering lags in the browser for a full text search across a large corpus. This is something I am incrementally exploring at this stage. The user experience will remain the same regardless, just the number of matches might increase.
@@ -29,7 +31,5 @@ The first two interactives are Altair graphs that allow a user to see how MPs fr
 
 
 ## Questions
-
-1. Do you anticipate rendering lags if I search a corpus of 300k+ rows x 200-300 word long text for a substring. The easiest way to do this would be to simply try, but if there are best practices to know beforehand, that would be helpful. I will also post on Ed.
-2. 
-3. 
+1. Do you anticipate rendering lags if I search a corpus of 300k+ rows x 200-300 word long text for a substring for the bar graph? The easiest way to do this would be to simply try, but if there are best practices to know beforehand, that would be helpful. I will also post on Ed.
+2. I am still trying to solve the issue of a single question (row) being asked by (corresponding to) multiple MPs (therefore multiple states, constituencies, etc). So far, I have retained only the first person (and state, constituency) who appears in the list, but this is misleading. One way to solve this is to make N distinct rows for such questions, one per MP. Do you have suggestions for any other way to handle this?
--- www/main.js
+++ www/main.js
@@ -1,17 +1,6 @@
-// create empty chart SVGs - pyramid charts start with a centered group
-// const dollarsG = makeChart("#pyramidDollars", 200)
-//   .append("g")
-//   .attr("transform", `translate(${chartWidth / 2}, ${margin.top})`);
-// const percentG = makeChart("#pyramidPercent", 200)
-//   .append("g")
-//   .attr("transform", `translate(${chartWidth / 2}, ${margin.top})`);
-// const lineDollars = makeChart("#lineChartDollars", 400);
-// const linePercent = makeChart("#lineChartPercent", 400);
-
 // Creates the empty SVG to be rendered upon page load
 const topicsBarChart = makeChart("#topicsChartContainer", 500, 300);
 
-// global state
 let questionsData = []; 
 // These are rough values for now, will update with precise values later.
 // Will also consider a rotating set of themes for comparison + handle the edge
@@ -23,23 +12,15 @@ topicFrequencies = [
     {topic: "nuclear", count: 768},
     {topic: "climate", count: 401},
 ]
-// let yearIndex = 0; // currently displayed year index
-// let timerFunc = null; // handle to timer function if animating
 
-// load CSV and parse data -- then draw charts for first time
+// Draws charts for the first time (bar chart has a static reference even before interaction)
 d3.csv("mini.csv").then((data) => {
   questionsData = data;
   updateCharts();
 });
 
-// called every frame of animation -- draw all charts
-// actual functions to draw charts are in other JS files (pyramid/linechart.js)
 function updateCharts() {
     drawTopicsChart(topicsBarChart)
-//   drawPyramid(dollarsG, yearIndex, "dollars");
-//   drawPyramid(percentG, yearIndex, "percent");
-//   drawLineChart(lineDollars, levelsData, "dollars");
-//   drawLineChart(linePercent, levelsData, "percent");
 }
 
 d3.select("#submitSearchQuery").on("click", function(event){
@@ -59,7 +40,7 @@ function countQueryOccurrences(query) {
 
 function drawTopicsChart(svg, query, count) {
 
-    let maxFreq = 2500;
+    let maxFreq = d3.max(topicFrequencies, topic => topic.count)
     if(query) {
         topicFrequencies.push({"topic": query, "count": count})
         topicFrequencies.sort((a, b) => b.count - a.count)
@@ -69,15 +50,15 @@ function drawTopicsChart(svg, query, count) {
         }    
     }
 
-    // Source: https://www.danvega.dev/blog/find-max-array-objects-javascript
-    
-    // let maxFreq = topicFrequencies.length == 0 ? 2500 : Math.max(...topicFrequencies.map(item => item.count));
-    // hardcoding the width and maxFreq for now -- the above code isn't working
-    
     let width_scale = d3.scaleLinear([0, maxFreq], [0, 500]);
-
-    svg.selectAll("#topicsChartContainer").exit().remove()
-
+    
+    svg.selectAll("#topicsChartContainer")
+    .exit()
+    .transition()
+    .duration(500)
+    .attr("opacity", 0)
+    .remove() //TODO: Fix this
+    
     svg.selectAll("#topicsChartContainer")
         .data(topicFrequencies)
         .enter() 
@@ -100,71 +81,9 @@ function drawTopicsChart(svg, query, count) {
     // Source: https://stackoverflow.com/questions/8668174/using-the-indexof-method-on-an-array-of-objects
     idx = topicFrequencies.map(item => item.topic).indexOf(query)
     svg.select(`#rect${idx}`)
-        .attr("style", "fill: #994e0d")
+        .attr("style", "fill: #994e0d") // TODO: This is not rendering correctly
    
     console.log(topicFrequencies)
-    // delete the queried term from the array
+    // delete the queried term from the array so it is not additive
     topicFrequencies = topicFrequencies.filter(item => item.topic !== query);
 }
-
-
-/*
-// disable timer (see below setInterval code)
-function stopTimer() {
-  clearInterval(timerFunc);
-  timerFunc = null;
-  d3.select("#playPause").text("Play");
-}
-
-// The below global code runs on page load to set up the application.
-
-// color the text in the paragraph to act as a key
-d3.selectAll(".pyramidLabel").style("color", (d, i) => d3.schemeOranges[5][i]);
-
-// load CSV and parse data -- then draw charts for first time
-d3.csv("levels.csv").then((data) => {
-  levelsData = data;
-  updateCharts();
-});
-
-// if they drag the slider, update charts
-d3.select("#timeSlider").on("change", function (e) {
-  // update the index & redraw
-  yearIndex = e.target.value;
-  updateCharts();
-});
-
-// if play button is clicked toggle play state
-d3.select("#playPause").on("click", function (e) {
-  // toggle the playing class -- used for display
-  // as well as knowing if the animation is active
-  e.target.classList.toggle("playing");
-
-  // if the state is now playing -- start a timer function
-  if (e.target.classList.contains("playing")) {
-    // toggle button to say 'Pause'
-    d3.select(e.target).text("Pause");
-
-    // reset timer if they've gone past the end
-    if (yearIndex >= levelsData.length) {
-      yearIndex = 0;
-    }
-
-    // setInterval will call this function every 100ms until cancelled
-    // the variable "timerFunc" stores a reference to the timer so we can
-    // pause/cancel it
-    timerFunc = setInterval(function () {
-      yearIndex += 1;
-      const timeSlider = d3.select("#timeSlider");
-      if (yearIndex >= levelsData.length && timerFunc) {
-        stopTimer();
-        yearIndex = levelsData.length - 1;
-      }
-      timeSlider.attr("value", yearIndex);
-      updateCharts();
-    }, 100);
-  } else if (timerFunc) {
-    stopTimer();
-  }
-});
-*/
